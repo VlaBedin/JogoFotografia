cdfa5efdae03bd44c87585f690b6b5379a682337
VlaBedin <69062155+VlaBedin@users.noreply.github.com>
2020-07-31 21:38:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JogoFotografia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
